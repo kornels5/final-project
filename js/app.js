3e4f20e0c33175f7c0f9bc4c1f21a9e97118a5f8
Kornel Sroka <srokakornel@gmail.com>
2018-03-13 15:59:41 +0100

    start/break countdown setup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
 	let num = document.querySelector('#num');
 	let breakNum = document.querySelector('#breakNum');
@@ -7,53 +7,64 @@ document.addEventListener("DOMContentLoaded", function() {
 	let breakTime = parseInt(breakNum.innerHTML);
 
 	let startBtn = document.querySelector('#start');
-	
+
 	let resetBtn = document.querySelector('#reset');
 	resetBtn.style.display = 'none';
 
-	startBtn.addEventListener('click', function() {
+	startBtn.addEventListener('click', function () {
 		let counter = setInterval(timer, 1000);
 
-		function timer(){
+		function timer() {
 
 			//hide variables
-			document.querySelectorAll('#minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #title1, #title2').forEach(function(element){
+			document.querySelectorAll('#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #title1, #title2').forEach(function (element) {
 				element.style.display = 'none';
 			});
 
 			document.querySelector('#timeType').innerHTML = `Session Time:`;
-			
+
 			count -= 1;
-			if(count === 0) {
+			if (count === 0) {
 				clearInterval(counter);
+				let startBreak = setInterval(breakTimer, 1000);
+				document.querySelector('#num').style.display = 'none';
+
+				function breakTimer() {
+					document.querySelector('#timeType').innerHTML = `Break Time:`;
+					document.querySelector('#breakNum').style.display = 'block';
+					breakTime -= 1;
+					if (breakTime === 0) {
+						clearInterval(startBreak);
+					}
+					document.querySelector('#breakNum').innerHTML = breakTime;
+				}
 			}
 			num.innerHTML = count;
 		}
-		
 	});
 
 	// setting +/- 5 buttons
 
-	document.querySelector('#minus5Clock').addEventListener('click', function () { 
-		if(count > 5) {
+	document.querySelector('#minus5Clock').addEventListener('click', function () {
+		if (count > 5) {
 			count -= 5;
 			num.innerHTML = count;
 		}
 	});
 
-	document.querySelector('#add5Clock').addEventListener('click', function () { 
+	document.querySelector('#add5Clock').addEventListener('click', function () {
 		count += 5;
 		num.innerHTML = count;
 	});
 
-	document.querySelector('#minus5Break').addEventListener('click', function () { 
-		if(breakTime > 5) {
+	document.querySelector('#minus5Break').addEventListener('click', function () {
+		if (breakTime > 5) {
 			breakTime -= 5;
 			breakNum.innerHTML = breakTime;
 		}
 	});
 
-	document.querySelector('#add5Break').addEventListener('click', function () { 
+	document.querySelector('#add5Break').addEventListener('click', function () {
 		breakTime += 5;
 		breakNum.innerHTML = breakTime;
 	});
