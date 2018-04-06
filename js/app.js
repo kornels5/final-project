a8c5158e93b5f809715af1b254e737001b2cb8cc
Kornel Sroka <srokakornel@gmail.com>
2018-04-06 21:12:44 +0200

    style

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
   //session time
-  
+
   let timeCounter = parseInt($('#workTime').html()),
     timeSeconds = timeCounter * 60,
     //break time
     breakTime = parseInt($('#breakTime').html()),
     breakSeconds = breakTime * 60,
-    //INTERVAL
+    //INTERVALS
     interval = 0,
+    intervalBreak = 0,
     //CURRENTLY DISPLAYED TIME ON POMODORRO
     currentTime = 0,
-    //CHECKS IF INTERVAL IS STILL RUNNING
-    alarm = $('#alarm')[0];
+    //ALARM
+    alarm = $('#alarm')[0],
+    //VARIABLE TO HOLD NAME OF CURRENTLY RUNNING TASK
+    workMode = '';
 
   //FUNCTION TO CONVERT SECONDS TO READABLE TIME
 
@@ -30,7 +33,7 @@ $(document).ready(function () {
   //SETTING TIME BUTTONS
 
   $('#minusClock').click(function () {
-    if (timeSeconds > 1) {
+    if (timeSeconds > 60) {
       timeSeconds -= 60;
       $('#workTime').html(humanReadableTime(timeSeconds));
     }
@@ -41,7 +44,7 @@ $(document).ready(function () {
   });
 
   $('#minusBreak').click(function () {
-    if (breakSeconds > 1) {
+    if (breakSeconds > 60) {
       breakSeconds -= 60;
       $('#breakTime').html(humanReadableTime(breakSeconds));
     }
@@ -52,14 +55,35 @@ $(document).ready(function () {
   });
 
 
-  function counter(seconds, ) {
+  function counter(seconds) {
     interval = setInterval(function () {
+      $('#title').html('work');
+      workMode = 'work';
       seconds -= 1;
       currentTime = seconds;
       $('#timer').html(humanReadableTime(seconds));
       if (seconds === 0) {
         alarm.play();
         clearInterval(interval);
+        counterBreak(breakSeconds);
+      }
+    }, 100);
+  }
+
+  function counterBreak(seconds) {
+    intervalBreak = setInterval(function () {
+      $('#title').html('break');
+      workMode = 'break';
+      seconds -= 1;
+      currentTime = seconds;
+      $('#timer').html(humanReadableTime(seconds));
+      if (seconds === 0) {
+        alarm.play();
+        clearInterval(intervalBreak);
+        workMode = '';
+        $('#start').show();
+        $('#pause, #resume').hide();
+        $('#title').html('');
       }
     }, 100);
   }
@@ -69,7 +93,6 @@ $(document).ready(function () {
   $('#pause, #resume').hide();
 
   $('#start').click(function () {
-    $('#settings').hide();
     $(this).hide();
     $('#pause, #resume').show();
     $('#timer').html(humanReadableTime(timeSeconds));
@@ -77,15 +100,52 @@ $(document).ready(function () {
   });
 
   $('#pause').click(function () {
-    clearInterval(interval);
+
+    $(this).attr('disabled', true);
+    $('#resume').removeAttr('disabled');
+    if (workMode === 'work') {
+      clearInterval(interval);
+    } else if (workMode === 'break') {
+      clearInterval(intervalBreak);
+    }
   });
 
   $('#resume').click(function () {
-    counter(currentTime);
+
+    $(this).attr('disabled', true);
+    $('#pause').removeAttr('disabled');
+    if (workMode === 'work') {
+      counter(currentTime);
+    } else if (workMode === 'break') {
+      counterBreak(currentTime);
+    }
   });
 
-  $('#toggleMenu').click(function(){
-    $('#settings').animate({width: 'toggle'});
+  //TASK LIST
+
+  $('#toggleMenu').click(function () {
+    $('#settings, #session, #break').animate({
+      width: 'toggle'
+    });
+  });
+
+  $('ul').on('click', 'li', function(){
+    $(this).toggleClass('completed');
+  });
+
+  $('ul').on('click', 'span', function(){
+    $(this).parent().fadeOut(500, function(){
+      $(this).remove();
+    });
+    event.stopPropagation();
+  });
+
+  $('input[type="text"]').keypress(function(event){
+    if(event.which === 13) {
+      let taskName = $(this).val();
+      $(this).val('');
+      $('ul').append(`<li><span class="remove"><i class="fas fa-trash-alt"></i></span>${taskName}</li>`);
+    }
   });
 
 });
